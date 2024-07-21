1a6db2aaf736c425c0f8c474345abfcf49484e18
juniorrafiu7 <juniorrafiu7@gmail.com>
2024-07-21 12:04:05 +0000

    hard

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,72 @@
 #!/usr/bin/python3
-""" """
+""" ALL THE TESTS"""
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+from models.base_model import BaseModel
+import unittest
+from os import getenv
+import pep8
+import models
+
+storage = getenv("HBNB_TYPE_STORAGE")
 
 
-class test_City(test_basemodel):
-    """ """
+class test_City_(unittest.TestCase):
+    """ UNITTEST CITY"""
+    @classmethod
+    def setUp(self):
+        """SetUp method"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+        self.city = City()
+        self.city.state_id = "AZ1"
+        self.city.name = "EL JADIDA"
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+    @classmethod
+    def TearDown(self):
+        """TearDown method."""
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        del self.city
+
+    def test_docstring(self):
+        """Test docstring for the module and the class"""
+
+        self.assertIsNotNone(
+            models.city.__doc__,
+            "No docstring in the module"
+        )
+        self.assertIsNotNone(City.__doc__, "No docstring in the class")
+
+    def test_pep8_style_check(self):
+        """Test pep8"""
+
+        style = pep8.StyleGuide(quiet=True)
+        s = style.check_files(['models/city.py'])
+        self.assertEqual(s.total_errors, 0, "pep8 error needs fixing")
+
+    def test_type_object(self):
+        """Test type object of city"""
+
+        self.assertEqual(
+            str(type(self.city)),
+            "<class 'models.city.City'>")
+        self.assertIsInstance(self.city, City)
+
+    def test_city_inheritence(self):
+        """checks if inherits"""
+
+        self.assertIsInstance(self.city, BaseModel)
+
+    def test_db_tbname(self):
+        """checks the tablename"""
+
+        self.assertEqual(self.city.__tablename__, "cities")
+
+    def test_attr(self):
+        """ checks the attributes"""
+
+        self.assertTrue("name" in self.city.__dir__())
+        if storage == 'db':
+            name_value = getattr(self.city, "name")
+            state_id = getattr(self.city, "state_id")
+            self.assertIsInstance(name_value, str)
+            self.assertEqual(state_id, str)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,74 @@
 #!/usr/bin/python3
-""" """
+""" ALL THE TESTS"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+from models.base_model import BaseModel
+import unittest
+from os import getenv
+import pep8
+import models
+from models.engine.db_storage import DBStorage
 
 
-class test_review(test_basemodel):
-    """ """
+class test_Review_(unittest.TestCase):
+    """ UNITTEST REVIEW"""
+    @classmethod
+    def setUp(self):
+        """SetUp method"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        self.review = Review()
+        self.review.user_id = "E4ZA"
+        self.review.place_id = "R1475"
+        self.review.text = "The consulktation was very helpful."
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+    @classmethod
+    def TearDown(self):
+        """TearDown method."""
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        del self.review
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+    def test_docstring(self):
+        """Test docstring for the module and the class"""
+
+        self.assertIsNotNone(
+            models.review.__doc__,
+            "No docstring in the module"
+        )
+        self.assertIsNotNone(Review.__doc__, "No docstring in the class")
+
+    def test_pep8_style_check(self):
+        """Test pep8"""
+
+        style = pep8.StyleGuide(quiet=True)
+        s = style.check_files(['models/review.py'])
+        self.assertEqual(s.total_errors, 0, "pep8 error needs fixing")
+
+    def test_type_object(self):
+        """Test type object of Review"""
+
+        self.assertEqual(
+            str(type(self.review)),
+            "<class 'models.review.Review'>")
+        self.assertIsInstance(self.review, Review)
+
+    def test_Review_inheritence(self):
+        """checks if inherits"""
+
+        self.assertIsInstance(self.review, BaseModel)
+
+    def test_db_tbname(self):
+        """checks the tablename"""
+
+        self.assertEqual(self.review.__tablename__, "reviews")
+
+    @unittest.skipIf(
+        type(models.storage) == DBStorage,
+        "Testing database storage only")
+    def test_Review_attributes(self):
+        """ check attr"""
+        place_id = getattr(self.review, "place_id")
+        self.assertIsInstance(place_id, str)
+        user_id = getattr(self.review, "user_id")
+        self.assertIsInstance(user_id, str)
+        text = getattr(self.review, "text")
+        self.assertIsInstance(text, str)
